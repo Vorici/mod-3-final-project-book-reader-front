4362be02b7faa40cecf7cae87eab75c5797c2e97
Ledio <L.Bode515@gmail.com>
2018-07-12 18:01:46 -0400

    index.js changes

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ const ADMIN_USER_NAME = 'Admin';
 const mainHTML            = document.getElementById("container");
 const bookSidebarHTML     = document.getElementById("sidebar");
 const bookPageHTML      = document.getElementById("book-page");
-const myBooksBtn          = document.getElementById("my-books");
-const allBooksBtn          = document.getElementById("all-books");
-const loginSearchFields = document.getElementById("login-search-fields")
+let myBooksBtn;
+let allBooksBtn;
+let searchField;
 
 var booksList;
 var usersList;
@@ -19,6 +19,10 @@ var currentUserId;
 var booksListFilter;
 const pagesList = [];
 let currentPageIndex;
+let booksListSearchValue;
+let titlesCount;
+let currentBookId;
+let currentBook;
 
 getBooksFromApi();
 getUsersFromApi();
@@ -42,7 +46,32 @@ function pushUsers(users) {
 
 function pushBooks(books) {
   booksList = books;
-  bookSidebarHTML.innerHTML = ""
+  bookSidebarHTML.innerHTML = `<div class="w3-container">
+    <h2>Book Reader</h2>
+    <button onclick="document.getElementById('id01').style.display='block'" class="w3-button w3-green w3-large">Login</button>
+
+    <div id="id01" class="w3-modal">
+      <div class="w3-modal-content w3-card-4 w3-animate-zoom" style="max-width:600px">
+
+        <div class="w3-center"><br>
+          <span onclick="document.getElementById('id01').style.display='none'" class="w3-button w3-xlarge w3-hover-red w3-display-topright" title="Close Modal">&times;</span>
+          <img src="https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png" alt="Avatar" style="width:30%" class="w3-circle w3-margin-top">
+        </div>
+
+          <div id="login-section" class="w3-section">
+            <label><b>Name</b></label>
+            <input id="input-name" class="w3-input w3-border w3-margin-bottom" type="text" placeholder="Enter Your Name" name="usrname" required>
+          <button data-action="login" class="w3-btn w3-block w3-teal">Enter</button>
+          </div>
+
+        <div class="w3-container w3-border-top w3-padding-16 w3-light-grey">
+          <button onclick="document.getElementById('id01').style.display='none'" type="button" class="w3-button w3-red">Cancel</button>
+                <span class="w3-right w3-padding w3-hide-small">Please provide a login name. We will automatically create an account for you if you do not have one! </span>
+        </div>
+
+      </div>
+    </div>
+  </div>`
 }
 
 function addUserToDatabase(name) {
@@ -59,21 +88,70 @@ function addUserToDatabase(name) {
 }
 
 function displaySideBooks(userId) {
-  bookSidebarHTML.innerHTML = ""
+  titlesCount = 0
+  bookSidebarHTML.innerHTML = `<h3 id="titles-count">Titles (${titlesCount} listed)</h3>
+  <fieldset>
+      <label>Select Book List</label>
+
+      <div>
+          <input type="radio" id="all-books" name="books-filter" />
+          <label for="all-books">All Books</label>
+      </div>
+
+      <div>
+          <input type="radio" id="my-books" name="books-filter" />
+          <label for="my-books">My Books</label>
+      </div>
+
+  </fieldset>`
   booksList.forEach(book => {
-      if ( (book.user.id === userId && booksListFilter === "My Books")
-            || booksListFilter === "All Books"  ) {
+    if ( (book.user.id === userId && booksListFilter === "My Books")
+          || booksListFilter === "All Books"  ) {
+      if (booksListSearchValue) {
+        let titleUpperCase = book.title.toUpperCase()
+        if (titleUpperCase.includes(booksListSearchValue.toUpperCase())) {
+          titlesCount++
+          bookSidebarHTML.innerHTML +=
+            `<ul class="list-group">
+            <li data-item="book title" data-id="${book.id}" class="list-group-item">${book.title}</li>
+            </ul>
+          </div>`
+        }
+      }
+      else {
+        titlesCount++
         bookSidebarHTML.innerHTML +=
           `<ul class="list-group">
-          <li data-item="book title" data-action="${book.id}" class="list-group-item">${book.title}</li>
+          <li data-item="book title" data-id="${book.id}" class="list-group-item">${book.title}</li>
           </ul>
         </div>`
       }
+    }
   })
+  const titlesCountElement = document.getElementById("titles-count")
+  titlesCountElement.innerText = `Titles (${titlesCount} listed)`
+
+  myBooksBtn           = document.getElementById("my-books");
+  allBooksBtn          = document.getElementById("all-books");
+  searchField    = document.getElementById("search-field")
 }
 
 function displayPage() {
-  bookPageHTML.innerHTML = `<img src="${ GOOGLE_DRIVE_URL + pagesList[currentPageIndex].file_id }">`
+  bookPageHTML.innerHTML = `
+    <button data-action="first">First</button>
+    <button data-action="previous">Previous</button>
+    <button data-action="next">Next</button>
+    <button data-action="last">Last</button>`
+
+    if (currentBook.user.id === currentUserId) {
+        bookPageHTML.innerHTML += `<button data-action="drop-book">Remove from My Books</button>`
+    }
+    else {
+      bookPageHTML.innerHTML += `<button data-action="add-book">Add to My Books</button>`
+    }
+
+    bookPageHTML.innerHTML += `<br><img src="${ GOOGLE_DRIVE_URL + pagesList[currentPageIndex].file_id }">`
+
 }
 
 function getPagesFromApi(book_id) {
@@ -89,21 +167,29 @@ function getPagesFromApi(book_id) {
   })
 }
 
+/*
+*******EVENT LOGIC**************************
+*/
 
 document.addEventListener('DOMContentLoaded', () => {
   document.body.addEventListener('click', function(event) {
 
+
     if (event.target.dataset.item === "book title") {
-      booksList.forEach(function(b) {
-        if (event.target.dataset.action === `${b.id}`) {
-            getPagesFromApi(b.id);
-        }
-      })
+      currentBookId = parseInt(event.target.dataset.id)
+      currentBook = booksList.find(book => (book.id === currentBookId))
+      getPagesFromApi(currentBookId)
+      // booksList.forEach(function(b) {
+      //   if (event.target.dataset.action === `${b.id}`) {
+      //       getPagesFromApi(b.id);
+      //   }
+      // })
     }
 
-    else if (event.target.id === "login-user") {
+    else if (event.target.dataset.action === "login") {
 
       event.preventDefault()
+
       var loginField = document.getElementById("input-name")
       var loginFieldValue = loginField.value
 
@@ -113,7 +199,6 @@ document.addEventListener('DOMContentLoaded', () => {
         addUserToDatabase(loginFieldValue).then(() => {
           fetch(USER_URL).then(r => r.json()).then(u => {
             console.log(u)
-            debugger
             currentUserId = usersList[usersList.length-1].id
             fetch(USER_URL)
               booksListFilter = "All Books"
@@ -133,21 +218,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
       }
 
-      loginSearchFields.innerHTML = `
+      searchField.innerHTML = `
           <label>Search</label>
           <input id="input-search" type="text"></input>
           `
-      loginSearchFields.addEventListener('keyup', event => {
+      searchField.addEventListener('keyup', event => {
         if (event.target.id === "input-search") {
-          
+          booksListSearchValue = document.getElementById("input-search").value
+          displaySideBooks(currentUserId);
         }
       })
 
     }
 
     else if (event.target.id === "my-books") {
-//      event.preventDefault()
-      console.log("My Books")
       booksListFilter = "My Books"
       displaySideBooks(currentUserId);
       myBooksBtn.checked = true;
@@ -155,12 +239,34 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     else if (event.target.id === "all-books") {
-//      event.preventDefault()
-      console.log("All Books")
       booksListFilter = "All Books"
       displaySideBooks(currentUserId);
       myBooksBtn.checked = false;
       allBooksBtn.checked = true;
+    }
+
+    else if (event.target.dataset.action === 'first') {
+      currentPageIndex = 0
+      displayPage()
+    }
+
+    else if (event.target.dataset.action === 'next') {
+      if (currentPageIndex < pagesList.length - 1) {
+        ++currentPageIndex
+        displayPage()
+      }
+    }
+
+    else if (event.target.dataset.action === 'previous') {
+      if (currentPageIndex) {
+        --currentPageIndex
+        displayPage()
+      }
+    }
+
+    else if (event.target.dataset.action === 'last') {
+      currentPageIndex = pagesList.length - 1
+      displayPage()
     }
 
   })
