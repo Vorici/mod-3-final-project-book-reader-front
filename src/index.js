0ec5adb0c70ab1e12c1458d199b481da0d375a57
Ledio <L.Bode515@gmail.com>
2018-07-13 09:40:43 -0400

    css stuff

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
-const BOOK_URL = "http://localhost:3000/api/v1/books/"
-const USER_URL = "http://localhost:3000/api/v1/users/"
-const PAGE_URL = "http://localhost:3000/api/v1/pages/"
-const GOOGLE_DRIVE_URL = "https://drive.google.com/uc?export=view&id="
+const BOOK_URL          = "http://localhost:3000/api/v1/books/"
+const USER_URL          = "http://localhost:3000/api/v1/users/"
+const PAGE_URL          = "http://localhost:3000/api/v1/pages/"
+const GOOGLE_DRIVE_URL  = "https://drive.google.com/uc?export=view&id="
 
-const ADMIN_USER_NAME = 'Admin';
+const ADMIN_USER_NAME   = 'Admin';
 
-const mainHTML            = document.getElementById("container");
-const bookSidebarHTML     = document.getElementById("sidebar");
+const pagesList = [];
+const mainHTML          = document.getElementById("container");
+const bookSidebarHTML   = document.getElementById("sidebar");
 const bookPageHTML      = document.getElementById("book-page");
+
 let myBooksBtn;
 let allBooksBtn;
 let searchField;
 
-var booksList;
-var usersList;
-var adminUserId;
-var currentUserId;
-var booksListFilter;
-const pagesList = [];
+let booksList;
+let usersList;
+let adminUserId;
+let currentUserId;
+let booksListFilter;
+
 let currentPageIndex;
 let booksListSearchValue;
 let titlesCount;
@@ -48,13 +50,13 @@ function pushBooks(books) {
   booksList = books;
   bookSidebarHTML.innerHTML = `<div class="w3-container">
     <h2>Book Reader</h2>
-    <button onclick="document.getElementById('id01').style.display='block'" class="w3-button w3-green w3-large">Login</button>
+    <button onclick="document.getElementById('modal').style.display='block'" class="w3-button w3-green w3-large">Login</button>
 
-    <div id="id01" class="w3-modal">
+    <div id="modal" class="w3-modal">
       <div class="w3-modal-content w3-card-4 w3-animate-zoom" style="max-width:600px">
 
         <div class="w3-center"><br>
-          <span onclick="document.getElementById('id01').style.display='none'" class="w3-button w3-xlarge w3-hover-red w3-display-topright" title="Close Modal">&times;</span>
+          <span onclick="document.getElementById('modal').style.display='none'" class="w3-button w3-xlarge w3-hover-red w3-display-topright" title="Close Modal">&times;</span>
           <img src="https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png" alt="Avatar" style="width:30%" class="w3-circle w3-margin-top">
         </div>
 
@@ -65,7 +67,7 @@ function pushBooks(books) {
           </div>
 
         <div class="w3-container w3-border-top w3-padding-16 w3-light-grey">
-          <button onclick="document.getElementById('id01').style.display='none'" type="button" class="w3-button w3-red">Cancel</button>
+          <button onclick="document.getElementById('modal').style.display='none'" type="button" class="w3-button w3-red">Cancel</button>
                 <span class="w3-right w3-padding w3-hide-small">Please provide a login name. We will automatically create an account for you if you do not have one! </span>
         </div>
 
@@ -89,21 +91,20 @@ function addUserToDatabase(name) {
 
 function displaySideBooks(userId) {
   titlesCount = 0
-  bookSidebarHTML.innerHTML = `<h3 id="titles-count">Titles (${titlesCount} listed)</h3>
-  <fieldset>
+  bookSidebarHTML.innerHTML = `</br><h3 id="titles-count">Titles (${titlesCount} listed)</h3>
+
       <label>Select Book List</label>
 
       <div>
-          <input type="radio" id="all-books" name="books-filter" />
+          <input class="ui toggle checkbox" type="radio" id="all-books" name="books-filter" />
           <label for="all-books">All Books</label>
       </div>
 
       <div>
-          <input type="radio" id="my-books" name="books-filter" />
+          <input class="ui toggle checkbox" type="radio" id="my-books" name="books-filter" />
           <label for="my-books">My Books</label>
-      </div>
+      </div></br>`
 
-  </fieldset>`
   booksList.forEach(book => {
     if ( (book.user.id === userId && booksListFilter === "My Books")
           || booksListFilter === "All Books"  ) {
@@ -112,45 +113,97 @@ function displaySideBooks(userId) {
         if (titleUpperCase.includes(booksListSearchValue.toUpperCase())) {
           titlesCount++
           bookSidebarHTML.innerHTML +=
-            `<ul class="list-group">
-            <li data-item="book title" data-id="${book.id}" class="list-group-item">${book.title}</li>
-            </ul>
+          `<div class="item">
+             <div data-item="book title" data-id="${book.id}"class="header">${book.title}</div>
+               ${book.authors}
           </div>`
         }
       }
       else {
         titlesCount++
         bookSidebarHTML.innerHTML +=
-          `<ul class="list-group">
-          <li data-item="book title" data-id="${book.id}" class="list-group-item">${book.title}</li>
-          </ul>
-        </div>`
+           `<div class="item">
+              <div data-item="book title" data-id="${book.id}"class="header">${book.title}</div>
+                ${book.authors}
+           </div>`
       }
     }
   })
-  const titlesCountElement = document.getElementById("titles-count")
+  const titlesCountElement     = document.getElementById("titles-count")
   titlesCountElement.innerText = `Titles (${titlesCount} listed)`
 
-  myBooksBtn           = document.getElementById("my-books");
-  allBooksBtn          = document.getElementById("all-books");
-  searchField    = document.getElementById("search-field")
+  myBooksBtn   = document.getElementById("my-books");
+  allBooksBtn  = document.getElementById("all-books");
+  searchField  = document.getElementById("search-field")
 }
+function persistBody(){
+  return document.body.innerHTML =  `<body>
+    <div class="container">
+    <div class="ui icon input"id="search-field">
+      <!-- <label>User</label>
+      <input id="input-name" type="text"></input>
+      <button data-action="login" id="login-user">Login</button> -->
+      <i class="inverted circular search link icon"></i>
+    </div>
 
+
+    <div class='row'>
+
+      <div id="sidebardiv" class='col-md-4'>
+        <div id="sidebar" class='ui selected list'>
+
+        </div>
+
+      </div>
+
+      <div class='col-md-7'>
+        <div id="book-page">
+        </div>
+      </div>
+    </div>
+  </div>
+      <script src="src/index.js" charset="utf-8"></script>
+  </body>`
+}
 function displayPage() {
-  bookPageHTML.innerHTML = `
-    <button data-action="first">First</button>
-    <button data-action="previous">Previous</button>
-    <button data-action="next">Next</button>
-    <button data-action="last">Last</button>`
+  // bookPageHTML.innerHTML = `
+  //   <button data-action="first">First</button>
+  //   <button data-action="last">Last</button>`
 
     if (currentBook.user.id === currentUserId) {
-        bookPageHTML.innerHTML += `<button data-action="drop-book">Remove from My Books</button>`
-    }
-    else {
-      bookPageHTML.innerHTML += `<button data-action="add-book">Add to My Books</button>`
+      bookPageHTML.innerHTML = `
+      <div data-action="drop-book" class="ui top attached button" tabindex="0">Remove Book</div>
+        <div class="ui attached segment">
+        <button data-action="previous" class="ui left attached button">Previous</button>
+          <img src="${ GOOGLE_DRIVE_URL + pagesList[currentPageIndex].file_id }">
+        <button data-action="next" class="right attached ui button">Next</button>
+        </div>
+    `
     }
 
-    bookPageHTML.innerHTML += `<br><img src="${ GOOGLE_DRIVE_URL + pagesList[currentPageIndex].file_id }">`
+
+
+
+    else {
+      // bookPageHTML.innerHTML += `<button data-action="add-book">Add to My Books</button>`
+      bookPageHTML.innerHTML = `
+
+
+      <div data-action="add-book" class="ui top attached button" tabindex="0">Add Book</div>
+        <div class="ui attached segment">
+        <button data-action="previous" class="ui left attached button">Previous</button>
+          <img src="${ GOOGLE_DRIVE_URL + pagesList[currentPageIndex].file_id }">
+        <button data-action="next" class="right attached ui button">Next</button>
+        </div>
+
+      `
+
+
+    }
+
+    // bookPageHTML.innerHTML += `<button data-action="previous" class="ui left attached button">Previous</button>
+    //   <img src="${ GOOGLE_DRIVE_URL + pagesList[currentPageIndex].file_id }">
+    //   <button data-action="next" class="right attached ui button">Next</button>`
 
 }
 
@@ -190,10 +243,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
       event.preventDefault()
 
-      var loginField = document.getElementById("input-name")
-      var loginFieldValue = loginField.value
+      let loginField = document.getElementById("input-name")
+      let loginFieldValue = loginField.value
 
-      var user = usersList.find(n => n.name === loginFieldValue)
+      let user = usersList.find(n => n.name === loginFieldValue)
 
       if (!user) {
         addUserToDatabase(loginFieldValue).then(() => {
@@ -219,8 +272,8 @@ document.addEventListener('DOMContentLoaded', () => {
       }
 
       searchField.innerHTML = `
-          <label>Search</label>
-          <input id="input-search" type="text"></input>
+          <input id="input-search" type="text" placeholder="Search..."></input>
+          <i class="inverted circular search link icon"></i>
           `
       searchField.addEventListener('keyup', event => {
         if (event.target.id === "input-search") {
